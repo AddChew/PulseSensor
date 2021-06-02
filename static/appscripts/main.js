bcd80cc7371ea386b248fb864786934c7d05ee62
AddChew <56240071+AddChew@users.noreply.github.com>
2021-06-02 23:18:58 +0800

    added dropdown

--- static/appscripts/main.js
+++ static/appscripts/main.js
@@ -1,7 +1,5 @@
 import {DragAndDrop, TextArea, Tabs, Progress, Table, Pages, ContainerElement} from "./jslibs/customElements.js"
 
-const fileReader = new FileReader()
-
 // Setup Single Tab
 
 // Setup Worker
@@ -37,6 +35,7 @@ let predict = () => {
 // Setup Batch Tab
 
 // Setup File Upload Page
+const fileReader = new FileReader()
 const dropAreaElement = document.querySelector("input")
 const dropArea = new DragAndDrop(dropAreaElement, ".csv")
 dropArea.element.addEventListener("change", () => {
@@ -49,7 +48,10 @@ dropArea.dropAreaElement.element.addEventListener("drop", () => {
 })
 
 // Setup Table Page
+const selectElement = document.querySelector("select")
 const tableElement = document.querySelector("table")
+const select = new Choices(selectElement, {shouldSort: false})
+selectElement.addEventListener("change", () => page.pages[1]._showButton("nextButton", selectElement.value))
 
 let loadTable = async file => {
     if (!file) return 
@@ -57,6 +59,8 @@ let loadTable = async file => {
     const fileArray = await readFileAsArray(file)
     const columns = fileArray.columns
     const table = new Table(tableElement, fileArray, columns, 5, true, "custom-table")
+    select.clearChoices()
+    select.setChoices(columns.map(column => ({value: column, label: column})))
 }
 
 let readFileAsURL = file => {
@@ -73,6 +77,14 @@ let readFileAsArray = async file => {
     return fileArray
 }
 
+// Setup Progress Bar Page
+const progressElement = document.querySelector(".progress")
+const progress = new Progress(progressElement, 0, "custom-progress")
+// setInterval(() => {
+//     if (progress.value === 100) progress.reset()
+//     else progress.update(1)
+// }, 100)
+
 // Setup Tabs
 const tabContents = document.querySelectorAll("[data-tabs]")
 const tabs = new Tabs("Single", ...tabContents)
@@ -81,6 +93,9 @@ const tabs = new Tabs("Single", ...tabContents)
 const pageElements = document.querySelectorAll("[data-pages]")
 const page = new Pages(...pageElements)
 page.pages[0]._showButtons(false)
+page.pages[1]._showButton("nextButton", false)
+page.pages[2]._showButton("backButton", false)
+page.pages[2]._showButton("nextButton", false)
 
 
 // function* splitIntoBatches(reviewsArray, batchSize = 64){
