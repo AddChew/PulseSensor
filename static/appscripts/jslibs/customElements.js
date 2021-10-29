b3a4b78bbe48f58dcad244adaf691725d9947cff
AddChew <56240071+AddChew@users.noreply.github.com>
2021-10-30 00:17:00 +0800

    removed previous version

--- static/appscripts/jslibs/customElements.js
+++
@@ -1,8 +0,0 @@
-import DragAndDrop from "./fileUploader.js"
-import TextArea from "./textArea.js"
-import Tabs from "./tabs.js"
-import Progress from "./progress.js"
-import Table from "./table.js"
-import Pages from "./pages.js"
-import {ContainerElement} from "./baseClasses/baseElements.js"
-export {DragAndDrop, TextArea, Tabs, Progress, Table, Pages, ContainerElement}
